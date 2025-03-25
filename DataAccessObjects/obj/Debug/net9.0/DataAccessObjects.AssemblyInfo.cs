37d760d961d0194ea2f5b1b9255e3f02bfb3669e
Ngoc Thach <ngocthachnguyen4399@gmail.com>
2025-03-25 20:52:59 +0700

    objs

--- DataAccessObjects/obj/Debug/net9.0/DataAccessObjects.AssemblyInfo.cs
+++ DataAccessObjects/obj/Debug/net9.0/DataAccessObjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14e7b9f6c4c1857a0c1ff35b5ecb0e8b7fddf58d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff88307e5d761ef4c64f342f93dec533b7d50821")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
