84d4bccbb7a05fb07199532808d9998b47c4b610
Ngoc Thach <ngocthachnguyen4399@gmail.com>
2025-03-27 00:01:22 +0700

    add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
+WORKDIR /source
+
+# Copy csproj and restore dependencies
+COPY *.sln .
+COPY MONKEY5_API/*.csproj ./MONKEY5_API/
+COPY BusinessObjects/*.csproj ./BusinessObjects/
+COPY DataAccessObjects/*.csproj ./DataAccessObjects/
+COPY Repositories/*.csproj ./Repositories/
+COPY Services/*.csproj ./Services/
+RUN dotnet restore
+
+# Copy all files and build
+COPY . .
+RUN dotnet publish -c Release -o /app
+
+# Build runtime image
+FROM mcr.microsoft.com/dotnet/aspnet:9.0
+WORKDIR /app
+COPY --from=build /app .
+ENTRYPOINT ["dotnet", "MONKEY5_API.dll"]
--- Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f01927aafa66c76f055ff24b86eeb70176bcc18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+889b1f19f2d87aa0f8c46ae8dc80a16f693b2f27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
